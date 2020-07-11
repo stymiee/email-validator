dd18f18f893d3d51fbfd0daf9de571f9bb836841
John Conde <stymiee@gmail.com>
2020-07-11 08:08:05 -0400

    Added complete example

---
+++ examples/fullValidation.php
@@ -0,0 +1,41 @@
+<?php
+
+use EmailValidator\EmailValidator;
+
+require('../vendor/autoload.php');
+
+$customDisposableEmailList = [
+    'example.com',
+];
+
+$bannedDomainList = [
+    'domain.com',
+];
+
+$testEmailAddresses = [
+    'test@gmail.com',
+    'test@hotmail.com',
+    'test@outlook.com',
+    'test@yahoo.com',
+    'test@domain.com',
+    'test@example.com',
+    'test@nobugmail.com',
+    'test@mxfuel.com',
+    'test@cellurl.com',
+    'test@10minutemail.com',
+];
+
+$config = [
+    'checkMxRecords' => true,
+    'checkBannedListedEmail' => true,
+    'checkDisposableEmail' => true,
+    'bannedList' => $bannedDomainList,
+    'disposableList' => $customDisposableEmailList,
+];
+$emailValidator = new EmailValidator($config);
+
+foreach($testEmailAddresses as $emailAddress) {
+    $emailIsValid = $emailValidator->validate($emailAddress);
+    echo  ($emailIsValid) ? 'Email is valid' : $emailValidator->getErrorReason();
+    echo PHP_EOL;
+}
